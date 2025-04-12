2add44d2fb5a53aa7844a0667971a2b885221942
gbach1703 <dbach17032006@gmail.com>
2025-04-12 07:54:23 +0700

    commit 3

--- main8.js
+++ main8.js
@@ -1,25 +1,42 @@
 // ví dụ về một hàm trả về một hàm
-function createGreeting(greeting) {
-  return function (name) {
-    return greeting + ", " + name + "!";
+// function createGreeting(greeting) {
+//   return function (name) {
+//     return greeting + ", " + name + "!";
+//   };
+// }
+
+// const sayHello = createGreeting("Hello");
+// const sayHi = createGreeting("Hi");
+
+// let name = prompt("name: ");
+// console.log(sayHello(name));
+// console.log(sayHi(name));
+
+// // phep cong
+// function sum(a) {
+//   return function (b) {
+//     return a + b;
+//   };
+// }
+
+// let so = sum(10);
+
+// let n = Number(prompt("n = "));
+// console.log(so(n));
+
+// ví dụ 2:
+function hamTinh(loai) {
+  return function (a, b) {
+    if (loai === "cong") {
+      return a + b;
+    } else if (loai === "tru") {
+      return a - b;
+    } else if (loai === "nhan") {
+      return a * b;
+    } else if (loai === "chia") {
+      return a / b;
+    }
   };
 }
-
-const sayHello = createGreeting("Hello");
-const sayHi = createGreeting("Hi");
-
-let name = prompt("name: ");
-console.log(sayHello(name));
-console.log(sayHi(name));
-
-// phep cong
-function sum(a) {
-  return function (b) {
-    return a + b;
-  };
-}
-
-let so = sum(10);
-
-let n = Number(prompt("n = "));
-console.log(so(n));
+const phepCong = hamTinh("cong");
+console.log(phepCong(17, 3));
